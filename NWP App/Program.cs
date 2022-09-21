4e84e7cfa3ecffec8b93fd6068d8b63412cc2e81
FanyaOff <73064979+FanyaOff@users.noreply.github.com>
2022-09-21 19:55:46 +0300

    📝 Log system

--- NWP App/Program.cs
+++ NWP App/Program.cs
@@ -9,8 +9,17 @@ namespace NWP_App
     internal class Program
     {
         static string values;
+        private const string logFile = "latest.log";
+
         static void Main(string[] args)
         {
+            if (File.Exists(logFile))
+            {
+                File.Delete(logFile);
+                File.WriteAllText(logFile, "----- NWP Log File ----\n");
+            } else
+                File.WriteAllText(logFile, "----- NWP Log File ----\n");
+
             Console.WriteLine("Starting check thread.");
             Console.WriteLine("If you want add this to obs, add new source Text, check the box named by Read from file and select created file");
             updateFile().GetAwaiter().GetResult();
@@ -20,19 +29,39 @@ namespace NWP_App
         {
             while (true)
             {
-                exit:
-                var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
-                var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
-                string newValues = $"{mediaProperties.Artist} - {mediaProperties.Title}";
-                if (values != newValues)
+                try
                 {
-                    Console.WriteLine($"Now Playing: {newValues}");
-                    File.WriteAllText("nowPlayingFile.txt", newValues);
-                    values = newValues;
-                    goto exit;
+                exit:
+                    var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+                    var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+                    string newValues = $"{mediaProperties.Artist} - {mediaProperties.Title}";
+                    if (values != newValues)
+                    {
+                        log($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} / INFO] Now Playing: {newValues}");
+                        Console.WriteLine($"Now Playing: {newValues}");
+                        log($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} / INFO] Writing song to txt file");
+                        File.WriteAllText("nowPlayingFile.txt", newValues);
+                        log($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} / INFO] Writed!");
+                        values = newValues;
+                        goto exit;
+                    }
+                }
+                catch (Exception e)
+                {
+                    log($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} / ERROR] {e}");
+                    Console.WriteLine("Program is crashed. Check latest.log file. If you have some questions/found bug, open issue on my github - https://github.com/FanyaOff/Obs-Now-Playing");
                 }
             }
         }
+
+        public static void log(string text)
+        {
+            using (StreamWriter w = File.AppendText(logFile))
+            {
+                w.WriteLine(text, "\n");
+            }
+        }
+
         private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() =>
             await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
 
